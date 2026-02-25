7579c007b9cf3c0ab8da78d4a43ee30ccdeb2fa0
qa-riccardo <tua-email@personale.com>
2026-02-26 00:11:39 +0100

    feat: barrel export

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export * from './api/jsonPlaceholderApi.js';
+export * from './api/httpClient.js';
+export * from './schemas/comment.js';
+export * from './schemas/post.js';
+export * from './schemas/user.js';
+export * from './utils/email.js';
+export * from './utils/errors.js';
